c7ee72f3b2f392e228133c9450c3440daf199f7d
bruno-ryan <nayr.onurb@gmail.com>
2022-04-17 11:39:49 -0300

    add javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-console.log('advice generator');
+const randomAdviceUrl = 'https://api.adviceslip.com/advice';
+const advice = document.getElementById('advice');
+const adviceId = document.getElementById('adviceId');
+const button = document.getElementById('button');
+
+getAdvice();
+
+async function getAdvice() {
+  eventHappening = true;
+  const response = await fetch(randomAdviceUrl, {
+    cache: "reload"
+  });
+  const data = await response.json();
+  let adviceIdData = data.slip.id;
+  let adviceData = data.slip.advice;
+  updateAdviceId(adviceIdData, adviceData);
+  console.log(data);
+  console.log(adviceIdData);
+}
+
+function updateAdviceId(id, adviceData) {
+  adviceId.innerText = ` #${id}`;
+  advice.innerText = `"${adviceData}"`;
+}
+
+button.addEventListener('click', () => {
+  adviceId.innerText = '';
+  advice.innerText = 'Loading'
+  setTimeout(getAdvice, 1000);
+})
